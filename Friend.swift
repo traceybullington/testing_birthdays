194a51aa3c011a3c1544a85148771a2860bed3c8
traceybullington <traceylbullington@gmail.com>
2025-07-10 14:23:39 -0500

    birthday app steps 1-3

---
+++ Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  testing_birthdays
+//
+//  Created by Tracey Bullington on 7/10/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
