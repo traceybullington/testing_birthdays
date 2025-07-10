90fcf72ad363cb7dd74b6ba98b395252d8cb6af7
traceybullington <traceylbullington@gmail.com>
2025-07-10 13:17:35 -0500

    Initial Commit

---
+++ testing_birthdays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testing_birthdays
+//
+//  Created by Tracey Bullington on 7/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testing_birthdays/testing_birthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_birthdaysApp.swift
+//  testing_birthdays
+//
+//  Created by Tracey Bullington on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct testing_birthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
